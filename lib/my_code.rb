e24a1ad1a2d01d995b7dba4ed4b666227c4551f1
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 18:02:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,3 +28,20 @@ def map_to_double(source_array)
   end
   return new_array
 end 
+
+def map_to_square(source_array)
+  counter = 0 
+  new_array = []
+  while counter < source_array.length do
+    new_array.push(source_array[counter] ** 2)
+    counter += 1
+    end
+  return new_array
+end
+
+
+
+
+
+
+
