f656c9f5afde3c9abe211217cac32e362704b730
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 17:59:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,12 @@ def map_to_no_change(source_array)
   end
   return new_array
 end
+
+def map_to_double(source_array)
+  counter = 0 
+  new_array = []
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * 2)
+  end
+  return new_array
+end 
