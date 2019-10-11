977d32491d9d6c0bf5f86d5eae1b13ad857e89fe
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 18:00:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,6 +24,7 @@ def map_to_double(source_array)
   new_array = []
   while counter < source_array.length do
     new_array.push(source_array[counter] * 2)
+    counter += 1
   end
   return new_array
 end 
