59f9a75625b9e99b90d69b09783915baea15d993
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 18:07:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ def map_to_square(source_array)
   return new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   counter = 0 
   sum = 0
   while counter < source_array.length do
