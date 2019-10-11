60989fbf338f93fe67bf69e06a75559c22c5931e
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 18:13:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,6 @@ end
 
 def reduce_to_all_true(source_array)
   counter = 0 
-  
   while counter < source_array.length do
     if !source_array[counter]
       return false
@@ -63,11 +62,9 @@ end
 
 def reduce_to_any_true(source_array)
   counter = 0 
-  
   while counter < source_array.length do 
     if source_array[counter]
       return true
-      
     end
     counter += 1
   end
