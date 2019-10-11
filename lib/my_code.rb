c708f17912592f64a6c66ccef62501772583a078
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-11 17:56:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_to_negativize(source_array)
+  counter = 0
+  new_array = []
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * -1)
+  
+  end
+  
+end
